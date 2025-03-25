aab900578b72482e9b83d24e65efd5072be86cef
ape-onesolar <ape@onesolar.energy>
2025-03-25 13:18:27 +0100

    misc changes and formatting

--- Makefile
+++ Makefile
@@ -2,8 +2,3 @@ virtualenv: ## Create virtual environment
 	#if [ ! -d venv ]; then pip install virtualenv; virtualenv venv -p python3.10; fi
 	if [ ! -d venv ]; then python3.11 -m venv venv; fi
 	echo "To activate the new environment run this: 'source venv/bin/activate' (in Linux) or this: 'source venv/Scripts/activate' (in Windows)"
-
-format: ## Format code using black
-	#format code
-	if [ -d venv ]; then . venv/*/activate; fi && \
-	black --line-length 120 **.python
--- utils/util.py
+++ utils/util.py
@@ -1,6 +1,7 @@
 from sklearn import metrics
 import numpy as np
 
+
 class AverageMeter(object):
     """Computes and stores the average and current value"""
 
@@ -20,15 +21,16 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
-def eval_metrix(true_label,pred_label):
-    MAE = metrics.mean_absolute_error(true_label,pred_label)
-    MAPE = metrics.mean_absolute_percentage_error(true_label,pred_label)
-    MSE = metrics.mean_squared_error(true_label,pred_label)
-    RMSE = np.sqrt(metrics.mean_squared_error(true_label,pred_label))
-    return [MAE,MAPE,MSE,RMSE]
+def eval_metrix(true_label, pred_label):
+    MAE = metrics.mean_absolute_error(true_label, pred_label)
+    MAPE = metrics.mean_absolute_percentage_error(true_label, pred_label)
+    MSE = metrics.mean_squared_error(true_label, pred_label)
+    RMSE = np.sqrt(metrics.mean_squared_error(true_label, pred_label))
+    return [MAE, MAPE, MSE, RMSE]
 
-def save_to_txt(save_name,string):
-    f = open(save_name,mode='a')
+
+def save_to_txt(save_name, string):
+    f = open(save_name, mode="a")
     f.write(string)
-    f.write('\n')
+    f.write("\n")
     f.close()
